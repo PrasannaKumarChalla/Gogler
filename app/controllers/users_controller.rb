eaa8fa552ea403e2cbb822e37b134231cc21dbfc
PrasannaKumarChalla <kumar.challa7893@gmail.com>
2016-03-13 04:02:27 -0400

    User CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user,only: [:index,:edit,:update,:destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def show
   	@user=User.find(params[:id])
     
@@ -19,6 +25,24 @@ def create
   	render 'new'
   end
 end
+  
+def edit
+end
+
+def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+def update
+ if @user.update_attributes(user_params)
+   flash[:success] = "Profile updated"
+      redirect_to @user
+ else
+  render 'edit'
+ end
+end
 
 private
 
@@ -26,4 +50,21 @@ private
 	    params.require(:user).permit(:name,:email,:password,:password_confirmation)
 	end
 
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger]="Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+      @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
+    end
+
+     def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,15 +6,12 @@ Minitest::Reporters.use!
 include ApplicationHelper
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-
-
-def is_logged_in?
-  !session[:user_id].nil?
-end
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Logs in a test user.
   def log_in_as(user, options = {})
