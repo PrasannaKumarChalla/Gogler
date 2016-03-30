61462f01173b3cbce683cf7a37e3fa875ea48d06
PrasannaKumarChalla <kumar.challa7893@gmail.com>
2016-03-30 02:15:17 -0400

    Completed password reset&email configuration except email service in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,11 @@ end
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
+   # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 
   # Sends password reset email.
   def send_password_reset_email
